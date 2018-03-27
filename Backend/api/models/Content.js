a8b1b505218a9513af3852288fd03a8ad1f70d71
Karim ElGhandour <kghandour@live.com>
2018-03-27 17:32:01 +0200

    Initial schema

---
+++ Backend/api/models/Content.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose');
+
+var contentSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ["Post","Link"],
+        default: "Post"
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: String,
+        required: true
+    },
+    views: {
+        type: Number,
+        default:0
+    },
+    rating: {
+        type: Number,
+        default:0
+    },
+    shares: {
+        type: Number,
+        default:0
+    },
+    rating: {
+        type: Number,
+        default:0
+    },
+    boost: {
+        type: Boolean,
+        default: false
+    },
+    userid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: Date
+});
+
+mongoose.model('Content', contentSchema);

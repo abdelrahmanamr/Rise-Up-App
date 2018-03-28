40301a0fc5bc35de06abf4e56a8408f345a5453c
Karim ElGhandour <kghandour@live.com>
2018-03-28 12:38:04 +0200

    Template for user component

--- Frontend/src/app/user/user.component.ts
+++ Frontend/src/app/user/user.component.ts
@@ -4,7 +4,10 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  template: `test test test test
+  template: `<ngx-main-layout>
+  <nb-menu [items]="menu"></nb-menu>
+  <router-outlet></router-outlet>
+  </ngx-main-layout>
   `
 })
 export class UserComponent {

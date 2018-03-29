19d1ab25e6a574905dbe83ac2394c4bc4fde1c60
Ahmed Akram <ahmed.akram32765@gmail.com>
2018-03-29 19:42:07 +0200

    Cleanup changes

--- Frontend/src/app/content/viewallcontents/viewallcontents.component.ts
+++ Frontend/src/app/content/viewallcontents/viewallcontents.component.ts
@@ -1,7 +1,68 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-items',
-  template: `Viewing all contents`
+  template: `<div class="container">
+  <table class="table table-responsive">
+  <thead>
+   <tr>
+     <th>Contents Table</th>
+   </tr>
+ </thead>
+ <tbody>
+
+ <tr>
+ <td>Title</td>
+ <td>Rating</td>
+ 
+ </tr>
+
+   <tr *ngFor="let content of contents">
+     <td>{{content.title}}</td>
+     <td>{{content.rating}}</td>
+    
+   <td><Button (click)="ViewContent(content._id)"> ShowContent </Button></td>  
+   </tr>          
+
+ </tbody>
+  </table>
+  </div>
+
+  <br>
+
+  
+
+  `
 })
-export class ViewAllContentsComponent {}
+export class ViewAllContentsComponent {
+  public contents:any[]=[];
+  
+  constructor(private httpClient: HttpClient,private router: Router) { }
+
+ngOnInit() {
+  this.ViewContents();
+  }
+
+  ViewContents(){
+    this.httpClient.get(environment.apiUrl +'Content/viewContents').subscribe(
+      res=>{  
+        this.contents = res['data'];       
+      }
+    );
+  }
+ 
+
+  ViewContent(ID: string){
+
+    localStorage.setItem("contentID",ID);
+    this.router.navigate(['/content/viewcontent']);
+
+
+  }
+
+
+}
--- Frontend/src/app/content/viewcontent/viewcontent.component.ts
+++ Frontend/src/app/content/viewcontent/viewcontent.component.ts
@@ -1,7 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , ViewChild } from '@angular/core';
+import {Router} from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-content-viewcontent',
-  template: `View content`
+  template: ` 
+  <div class="container">
+  <span> <p> {{ Content }} </p> </span>
+  <span><a href="{{ Title }}">{{ Title1 }}</a></span> 
+  <span><img src="{{ImagePath}}">  </span>  
+  </div>
+  `
 })
-export class ViewContentComponent {}
+export class ViewContentComponent {
+  
+ID:string=localStorage.getItem("contentID");
+Content : any;
+Title:any
+Title1:any
+ImagePath:string
+
+  constructor(private httpClient: HttpClient,private router: Router,private domSanitizer: DomSanitizer) { }
+
+
+  ngOnInit() { 
+    this.GetContent(this.ID) ;
+      }
+
+
+  GetContent(ID:string){
+        var config ={
+      headers : 
+    {
+  'Content-Type':'application/json'
+    }
+  }
+    this.httpClient.get(environment.apiUrl +'/Content/viewContent/'+ID,config).subscribe(
+      res=>{  
+      if(res['data'].type== "Post"){
+        this.ViewText(this.ID)
+      }   
+      
+      if(res['data'].type== "Image"){
+        this.ViewImage(this.ID)
+      }  
+      if(res['data'].type== "Link"){
+        this.ViewLink(this.ID)
+      }  
+          
+      }
+    );
+ }
+
+     ViewText(ID:String){
+      var config ={
+        headers : 
+      {
+    'Content-Type':'application/json'
+      }
+    }
+      this.httpClient.get(environment.apiUrl +'/Content/viewContent/'+ID,config).subscribe(
+        res=>{  
+          this.Content = res['data'].body;  
+            
+        }
+      );
+     }
+
+     
+
+     ViewLink(ID:string){
+      var config ={
+        headers : 
+      {
+    'Content-Type':'application/json'
+      }
+    }
+      this.httpClient.get(environment.apiUrl +'/Content/viewContent/'+ID,config).subscribe(
+        res=>{  
+          this.Title1=res['data'].title
+          this.Title = res['data'].body;  
+            
+        }
+      );
+     }
+
+     ViewImage(ID:string){
+      var config ={
+        headers : 
+      {
+    'Content-Type':'application/json'
+      }
+    }
+      this.httpClient.get(environment.apiUrl +'/Content/viewContent/'+ID,config).subscribe(
+        res=>{  
+         this.ImagePath=(res['data'].body);      
+        }
+      );
+     }
+
+    
+
+
+
+
+}

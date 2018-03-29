33bd9226f6887148123b00bb625a265a56b3c707
ziyadk1997 <ziyadk@windowslive.com>
2018-03-29 14:18:48 +0200

    Added Delete Content to Content Controller and added routes to index.js

--- Backend/api/controllers/ContentController.js
+++ Backend/api/controllers/ContentController.js
@@ -16,4 +16,30 @@ module.exports.getContents = function(req, res, next) {
     });
 };
 
-
+module.exports.DeleteContent = function(req, res, next) {
+    if (!Validations.isObjectId(req.params.contentId)) {
+        return res.status(422).json({
+            err: null,
+            msg: 'contentId parameter must be a valid ObjectId.',
+            data: null
+        });
+    }
+    Content.findByIdAndRemove(req.params.contentId).exec(function(
+        err,
+        deletedContent
+    ) {
+        if (err) {
+            return next(err);
+        }
+        if (!deletedContent) {
+            return res
+                .status(404)
+                .json({ err: null, msg: 'Content not found.', data: null });
+        }
+        res.status(200).json({
+            err: null,
+            msg: 'Content was deleted successfully.',
+            data: deletedContent
+        });
+    });
+};
--- Backend/api/routes/index.js
+++ Backend/api/routes/index.js
@@ -10,7 +10,6 @@ router.patch('/admin/blockUser/:userId', AdminController.BlockUser);
 router.delete('/admin/removeCompany/:companyId',AdminController.RemoveCompany);
 
 //------------------------------Content Routes-----------------------------------
-router.get('/content/getContent',ContentCrtl.getContents);
-
+router.get('/content/getContents',ContentCrtl.getContents);
+router.delete('/content/removeContent',ContentCrtl.DeleteContent);
 module.exports = router;
-

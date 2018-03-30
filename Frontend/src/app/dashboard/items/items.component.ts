bae96b3540d60c1fdd921c9273b5a712c183a58b
ziyadk1997 <ziyadk@windowslive.com>
2018-03-30 14:19:43 +0200

    Revert "added delete company"
    This reverts commit 414385ee14f80d451746c72f64541f420928699e.

--- Frontend/src/app/dashboard/items/items.component.ts
+++ Frontend/src/app/dashboard/items/items.component.ts
@@ -5,80 +5,19 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-dashboard-items',
   template: ``
- 
- 
+
+
 })
 export class ItemsComponent {
 
-  ngOnInit() 
+  ngOnInit()
   {
-      
 
-       
+
+
   }
 
 
 
 
 }
-<<<<<<< HEAD
-
-</style>
-</head>
-
-<body>
-
-  
-
-<table id="products">
-  <tr>
-    <th>Name</th>
-    <th>Website</th>
-    <th>Seller Name</th>
-    <th>Created At</th>
-    <th>Updated At</th>
-    <th></th>
-    <th></th>
-  </tr>
-  
-<tr *ngFor="let item of data">
-  <td>{{item.name}}</td>
-  <td>{{item.price}}</td>
-  <td>{{item.sellerName}}</td>
-  <td>{{item.createdAt}}</td>
-  <td>{{item.updatedAt}}</td>
-  <td><button type="button" class="btn btn-default" (click)="delete(item._id)">Delete</button></td>
-  <td><button type="button" class="btn btn-default" (click)="edit(item._id)">Update</button></td>
-
-  </tr>
-</table>
-<button type="button" style="width:10" class="btn btn-default" (click)="add()">Add A Company</button>
-
-
-</body>
-</html>`
-})
-export class ItemsComponent {
-  data =[];
-  constructor(private http: HttpClient,private router: Router){}
-
-  ngOnInit() 
-  {
-       this.http.get('http://localhost:3000/api/admin/viewCompanies').
-       subscribe(res =>{this.data=res["data"]});
-  }
-  
-  
-  add()
-  {
-    window.location.replace("#/dashboard/addcompany");
-    
-  }
-
-  delete(id : string)
-  {
-    this.http.delete('http://localhost:3000/api/admin/removeCompany/'+id).subscribe(res=>{window.location.reload()});
-  }
-}
-=======
->>>>>>> 552d6ba99f494b26bc26481798c7829ef27ae95f

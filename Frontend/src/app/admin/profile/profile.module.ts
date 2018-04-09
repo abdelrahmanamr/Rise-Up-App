3d557a87c109de89170f32556dd7a06664ad18eb
salmaosamaelkholy <salmaosama@outlook.com>
2018-04-10 00:20:55 +0200

    Finished a part of editing tags of an expert

--- Frontend/src/app/admin/profile/profile.module.ts
+++ Frontend/src/app/admin/profile/profile.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { ProfileRoutingModule } from './profile-routing.module';
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { TagsInputModule } from 'ngx-tags-input/dist';
 import { ProfileComponent } from './profile.component';
 
 @NgModule({
-  imports: [ThemeModule, ProfileRoutingModule],
+  imports: [ThemeModule, ProfileRoutingModule,TagsInputModule.forRoot(),FormsModule],
   declarations: [ProfileComponent],
   providers: []
 })

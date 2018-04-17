430861bc9312096c3b444218512548c0e931d1ef
Karim ElGhandour <kghandour@live.com>
2018-04-17 15:59:40 +0200

    To HTML

--- Frontend/src/app/expert/viewallexperts/viewallexperts.component.ts
+++ Frontend/src/app/expert/viewallexperts/viewallexperts.component.ts
@@ -6,23 +6,7 @@ import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-expert-viewallexperts',
-  template: `
-  
-  <div class="container">
-  <div *ngFor="let expert of experts">
-    <div class="card" style="padding:10px 15px; padding-bottom:120px; margin-bottom:20px;display: block; ">
-    <div style="float:left; width:800px;">
-    <img src="/assets/profile1.png" style="float:left;margin-right:10px;">
-
-    <h4>{{expert.firstname}} {{expert.lastname}}</h4>
-    Email: {{expert.email}}<br />
-    Tags: <span class="tags-input__tag" *ngFor="let tag of expert.tags;">{{tag}}</span></div>
-    <div style="float:right;"><Button (click)="ViewExpert(expert._id)" class="btn btn-danger btn-sm"> View Expert </Button><br />
-
-    </div>
-  </div>
-  </div>
-  `
+  templateUrl: `viewallexperts.html`
 })
 export class ViewAllExpertsComponent {
 

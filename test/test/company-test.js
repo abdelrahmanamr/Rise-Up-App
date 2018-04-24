4f61ab103aeddfce284036c48bed27ff070eff61
ziyadk1997 <ziyadk@windowslive.com>
2018-04-24 16:57:43 +0200

    added company test

---
+++ test/test/company-test.js
@@ -0,0 +1,38 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+process.env.NODE_ENV = 'test';
+var mongoose = require("mongoose");
+var server = require('../../Backend/app');
+var User = require("../../Backend/api/models/User");
+var Company = require("../../Backend/api/models/Company");
+var should = chai.should();
+
+dbURI = 'mongodb://localhost:27017/nodejs-test';
+
+
+chai.use(chaiHttp);
+
+process.env.NODE_ENV = 'test';
+
+let base = undefined;
+if (!process.env.PWD) {
+  base = process.cwd();
+}else{
+  base = process.env.PWD;
+}
+var mongoose = require('mongoose'),
+  Admin = mongoose.model('Admin'),
+  should = require('should'),
+  chai = require('chai'),
+  chaiHTTP = require('chai-http'),
+  server = require(base +'/app'),
+  Teacher = mongoose.model('Teacher');
+chai.use(chaiHTTP);
+
+
+
+
+describe('Testing Search',function(){
+
+
+});

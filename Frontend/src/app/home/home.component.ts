429b6e65fabbb3966d812a6c500606f1ab121449
salmaosamaelkholy <salmaosama@outlook.com>
2018-04-26 01:00:56 +0200

    updated the UI of the homepage

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -8,140 +8,45 @@ import { NbMenuItem } from '@nebular/theme/components/menu/menu.service';
   template: `
   <ngx-main-layout >
   <router-outlet>
-  <!DOCTYPE html>
-  <html>
-  <title>W3.CSS</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <body>
-  <div class="container">
-  <img src="/assets/rise.jpg"  width="100%" height="300">
+  <div class="rightbg">
+  <div>
+  <img src="/assets/grey.png"  width="80%" height="30px">
+  
 </div>
-<div id="salma" class="w3-content w3-section" style="max-width:500px">
+  <div>
+  <img src="/assets/connect.png"  align="right" width="90px" height="400px">
   
-    <img class="mySlides w3-animate-fading" src="/assets/white.jpg" style="width:600px;height:75px">
-
-    
-
-  </div>
-  <div id="salma" class="w3-content w3-section" style="max-width:500px">
+</div>
+<div>
+  <img src="/assets/grey.png"  width="80%" height="65px">
   
-    <img class="mySlides w3-animate-fading" src="/assets/1.png" style="width:600px;height:264px">
+</div>
 
-
-  </div>
+  <div>
   
-  <script>
-  var myIndex = 0;
-  carousel();
-  
-  function carousel() {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";  
-      }
-      myIndex++;
-      if (myIndex > x.length) {myIndex = 1}    
-      x[myIndex-1].style.display = "block";  
-      setTimeout(carousel, 9000);    
-  }
-  </script>
-  <div id="salma" class="w3-content w3-section" style="max-width:500px">
-  
-    <img class="mySlides w3-animate-fading" src="/assets/white.jpg" style="width:600px;height:264px">
+  <img  src="/assets/final.gif" style="width:55%;height:20%">
 
-
-  </div>
-
-  <div id="salma" class="w3-content w3-section" style="max-width:500px">
   
-    <img class="mySlides w3-animate-fading" src="/assets/2.png" style="width:600px;height:264px">
 
-    
-
-  </div>
+</div>
+<div>
+  <img src="/assets/grey.png"  width="80%" height="200">
   
-  <script>
-  var myIndex = 0;
-  carousel();
-  
-  function carousel() {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";  
-      }
-      myIndex++;
-      if (myIndex > x.length) {myIndex = 1}    
-      x[myIndex-1].style.display = "block";  
-      setTimeout(carousel, 9000);    
-  }
-  </script>
-  <div id="salma" class="w3-content w3-section" style="max-width:500px">
-  
-    <img class="mySlides w3-animate-fading" src="/assets/white.jpg" style="width:600px;height:264px">
-
-
-  </div>
-  <div id="access" class="w3-content w3-section" style="max-width:500px">
-  
-    <img class="mySlides w3-animate-fading" src="/assets/3.png" style="width:600px;height:264px">
-
-  </div>
-  
-  <script>
-  var myIndex = 0;
-  carousel();
-  
-  function carousel() {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";  
-      }
-      myIndex++;
-      if (myIndex > x.length) {myIndex = 1}    
-      x[myIndex-1].style.display = "block";  
-      setTimeout(carousel, 9000);    
-  }
-  </script>
-  <div id="salma" class="w3-content w3-section" style="max-width:500px">
-  
-    <img class="mySlides w3-animate-fading" src="/assets/white.jpg" style="width:600px;height:264px">
-
-
-  </div>
-  <div class="container">
-  <iframe  width="1000" height="600" src="https://www.youtube.com/embed/-fQQxIa7mDc">
+</div>
+<div>
+  <iframe  width="80%" height="400px" src="https://www.youtube.com/embed/-fQQxIa7mDc">
 </iframe>
  </div>
-
-  </body>
-  <style>
-  #vid{
-    position:relative;
-  }
-  .container {
-    position: relative;
-}
-
-
-
-img { 
-    width: 100%;
-    height: auto;
-}
+  </div>
   
-  </style>
-
-  </html>
+ 
   </router-outlet>
   </ngx-main-layout>
-  `
+  `,
+  styles: [``]
+
 })
 export class HomeComponent implements OnInit {
-  menu: NbMenuItem[];
 
   ngOnInit() {
    
